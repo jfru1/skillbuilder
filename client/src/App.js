bf4f1b6b74f35da174a2ea210f1caa18d676aafe
mmegarba <micaeljohnmegarbane@gmail.com>
2017-10-20 13:30:30 -0400

    added login and signUp UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from "./components/Nav";
 import userSkill from "./pages/userSkill"
 import Contact from "./pages/Contact"
 import About from "./pages/About"
+import SignUpPage from "./pages/SignUp.js"
+import LoginPage from "./pages/Login.js"
 import Navbar from "./components/Navbar/Navbar.js";
 import Footer from "./components/Footer/Footer.js"
 import Wrapper from "./components/Wrapper/Wrapper.js"
@@ -18,10 +19,14 @@ const App = () =>
     <div>
       <Navbar />
       <Wrapper>
+
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/userSkill" component={userSkill} />
         <Route exact path="/contact" component={Contact} />
+        <Route exact path="/signUp" component ={SignUpPage} />
+        <Route exact path="/LoginIn" component ={LoginPage} />
+
       </Wrapper>
       <Footer />
     </div>
--- client/src/pages/userSkill.js
+++ client/src/pages/userSkill.js
@@ -2,7 +2,40 @@ import React from "react";
 
 const userSkill = () =>
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+     <div class="panel panel-default">
+        <div class="panel-heading">
+          <h1 class="panel-title"><strong>Define your Skill</strong></h1>
+        </div>
+        <div class="panel-body pItems row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="name">What do you want to learn?</label>
+              <input type="string" class="form-control" placeholder="I want to learn how to..."/>
+            </div>
+            <div class="form-group">
+              <label for="">Do some research, what is your goal?</label>
+              <input type="email" class="form-control" placeholder="After 20 hours, I want to..."/>
+            </div>
+            <div class="form-group">
+              <label for="msg">Break your path down into five clear steps.</label>
+              <textarea class="form-control" rows="1" placeholder="The first step is..."></textarea>
+              <textarea class="form-control" rows="1" placeholder="The second step is..."></textarea>
+              <textarea class="form-control" rows="1" placeholder="The third step is..."></textarea>
+              <textarea class="form-control" rows="1" placeholder="The fourth step is..."></textarea>
+              <textarea class="form-control" rows="1" placeholder="The final step is..."></textarea>
+            </div>
+            <div class="form-group">
+            <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+          </div>
+        </div>
+     </div>
+    </div>
 
-<div> BLAH BLAH! </div>
+</div>
+</div>
 
 export default userSkill;
