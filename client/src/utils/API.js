63794c4e3edafc1963c95119db593fb42df34007
mmegarba <michaeljohnmegarbane@gmail.com>
2017-11-08 00:20:40 +0200

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,7 +8,6 @@ export default {
     return axios({
 method:"post",
 url:"/api/saveClient",
-baseURL:process.env.AXIOS_API,
 data:clientData
     })
 
@@ -20,7 +19,6 @@ data:clientData
     return axios({
 method:"post",
 url:"/api/checkClient",
-baseURL:process.env.AXIOS_API,
 data:client
 
     })
@@ -32,7 +30,6 @@ saveSkills: function(clientSkill){
 return axios({
   method:"post",
   url:"/api/addClientSkill",
-  baseURL:process.env.AXIOS_API,
   data:clientSkill
 
 
@@ -47,7 +44,6 @@ addPost: function(clientPost){
 return axios({
   method:"post",
   url:"/api/addClientPost",
-  baseURL:process.env.AXIOS_API,
   data:clientPost
 
 
@@ -61,7 +57,6 @@ checkPost: function(clientEmail){
 return axios({
   method:"post",
   url:"/api/checkClientPost",
-  baseURL:process.env.AXIOS_API,
   data:clientEmail
 })
 
