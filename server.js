ed1d85feb8fa386d0202f77050683294c0cdfd0f
mmegarba <micaeljohnmegarbane@gmail.com>
2018-08-05 13:44:01 -0700

    fixing the routing

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use("/api", apiRoutes);
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, "client/public/index.html"), function(err) {
+  res.sendFile(path.join(__dirname, "index.html"), function(err) {
     if (err) {
-      console.log("error")
+      
       res.status(500).send(err)
     }
   })
